182a111ee88cdaa6b4d91f7ba86cb980523276e4
Ryan Allen <rallen3882@gmail.com>
2020-11-20 11:36:52 -0500

    Scraping challenges as well as advancements

--- src/test/java/com/github/ubunfu/mcbountyscraper/service/LogScraperServiceImplTest.java
+++ src/test/java/com/github/ubunfu/mcbountyscraper/service/LogScraperServiceImplTest.java
@@ -17,7 +17,9 @@ public class LogScraperServiceImplTest {
 
     private static final String PLAYER = "PLAYER_001";
     private static final String ACHIEVEMENT = "Who's the Pillager Now?";
+    private static final String CHALLENGE = ACHIEVEMENT;
     private static final String MESSAGE_ACHIEVEMENT = "[01:27:02] [Server thread/INFO]: " + PLAYER + " has made the advancement [" + ACHIEVEMENT + "]";
+    private static final String MESSAGE_CHALLENGE = "[01:27:02] [Server thread/INFO]: " + PLAYER + " has completed the challenge [" + CHALLENGE + "]";
     private static final String MESSAGE_NO_ACHIEVEMENT = "This message has no bounty";
 
     private LogScraperService logScraperService;
@@ -34,6 +36,12 @@ public class LogScraperServiceImplTest {
     void callsBountyProcessorForAchievementEarned() {
         logScraperService.handleLog(MESSAGE_ACHIEVEMENT);
         verify(bountyProcessorClient).postBounty(new BountyProcessorRequest(ACHIEVEMENT, PLAYER));
+    }
+
+    @Test
+    void callsBountyProcessorForChallengeCompleted() {
+        logScraperService.handleLog(MESSAGE_CHALLENGE);
+        verify(bountyProcessorClient).postBounty(new BountyProcessorRequest(CHALLENGE, PLAYER));
     }
 
     @Test
