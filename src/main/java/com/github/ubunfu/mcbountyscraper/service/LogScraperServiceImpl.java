0233607025ba24154759ed5a62861c5b29a497a4
Ryan Allen <rallen3882@gmail.com>
2021-12-18 11:05:27 -0500

    Code formatting to GoogleStyle

--- src/main/java/com/github/ubunfu/mcbountyscraper/service/LogScraperServiceImpl.java
+++ src/main/java/com/github/ubunfu/mcbountyscraper/service/LogScraperServiceImpl.java
@@ -1,5 +1,7 @@
 package com.github.ubunfu.mcbountyscraper.service;
 
+import static java.lang.String.format;
+
 import com.github.ubunfu.mcbountyscraper.client.BountyProcessorClient;
 import com.github.ubunfu.mcbountyscraper.client.BountyProcessorRequest;
 import feign.Response;
@@ -8,56 +10,52 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import static java.lang.String.format;
-
 @Service
 public class LogScraperServiceImpl implements LogScraperService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(LogScraperServiceImpl.class);
-    private static final String MATCHER_ACHIEVEMENT = "has made the advancement";
-    private static final String MATCHER_CHALLENGE = "has completed the challenge";
-    private static final String LEFT_SQUARE_BRACKET = "[";
-    private static final String RIGHT_SQUARE_BRACKET = "]";
-    private static final String SPACE = " ";
+  private static final Logger LOGGER = LoggerFactory.getLogger(LogScraperServiceImpl.class);
+  private static final String MATCHER_ACHIEVEMENT = "has made the advancement";
+  private static final String MATCHER_CHALLENGE = "has completed the challenge";
+  private static final String LEFT_SQUARE_BRACKET = "[";
+  private static final String RIGHT_SQUARE_BRACKET = "]";
+  private static final String SPACE = " ";
 
-    private BountyProcessorClient bountyProcessorClient;
+  private final BountyProcessorClient bountyProcessorClient;
 
-    @Autowired
-    public LogScraperServiceImpl(BountyProcessorClient bountyProcessorClient) {
-        this.bountyProcessorClient = bountyProcessorClient;
+  @Autowired
+  public LogScraperServiceImpl(BountyProcessorClient bountyProcessorClient) {
+    this.bountyProcessorClient = bountyProcessorClient;
+  }
+
+  @Override
+  public void handleLog(String log) {
+    if (log.contains(MATCHER_ACHIEVEMENT) || log.contains(MATCHER_CHALLENGE)) {
+      try {
+        Response response = bountyProcessorClient.postBounty(buildBountyProcessorRequest(log));
+        LOGGER.info(format("Discord response: %s", response.status()));
+      } catch (RuntimeException e) {
+        LOGGER.error(format("Error invoking Bounty Processor: %s, caused by: %s", e, e.getCause()));
+      }
     }
+  }
 
-    @Override
-    public void handleLog(String log) {
-        if (log.contains(MATCHER_ACHIEVEMENT) || log.contains(MATCHER_CHALLENGE)) {
-            try {
-                Response response = bountyProcessorClient.postBounty(buildBountyProcessorRequest(log));
-                LOGGER.info(format("Discord response: %s", response.status()));
-            } catch (RuntimeException e) {
-                LOGGER.error(format("Error invoking Bounty Processor: %s, caused by: %s", e, e.getCause()));
-            }
-        }
-    }
+  private BountyProcessorRequest buildBountyProcessorRequest(String message) {
+    message = stripServerInfo(message);
+    String playerName = parsePlayerName(message);
+    String achievement = parseAchievement(message);
+    return new BountyProcessorRequest(achievement, playerName);
+  }
 
-    private BountyProcessorRequest buildBountyProcessorRequest(String message) {
-        message = stripServerInfo(message);
-        String playerName = parsePlayerName(message);
-        String achievement = parseAchievement(message);
-        return new BountyProcessorRequest(achievement, playerName);
-    }
+  private String parseAchievement(String message) {
+    return message.substring(
+        message.lastIndexOf(LEFT_SQUARE_BRACKET) + 1, message.lastIndexOf(RIGHT_SQUARE_BRACKET));
+  }
 
-    private String parseAchievement(String message) {
-        return message.substring(
-                message.lastIndexOf(LEFT_SQUARE_BRACKET) + 1,
-                message.lastIndexOf(RIGHT_SQUARE_BRACKET)
-        );
-    }
+  private String parsePlayerName(String message) {
+    return message.split(SPACE)[0];
+  }
 
-    private String parsePlayerName(String message) {
-        return message.split(SPACE)[0];
-    }
-
-    private String stripServerInfo(String message) {
-        return message.substring(33);
-    }
+  private String stripServerInfo(String message) {
+    return message.substring(33);
+  }
 }
